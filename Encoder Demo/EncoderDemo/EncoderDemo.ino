b675a39116ca7a3409d090d9fcb69d99ac5c1980
Derek Schumacher <schumacher15@up.edu>
2015-03-25 11:07:28 -0700

    Added modified encoder demo to support index

---
+++ Encoder Demo/EncoderDemo/EncoderDemo.ino
@@ -0,0 +1,91 @@
+/****************************************************************************************
+
+Author:    Brenda A Bell
+Modified by Derek Schumacher on March 24, 2015
+
+****************************************************************************************/
+
+#define ENCODER0PINA         3      // this pin needs to support interrupts
+#define ENCODER0PINB         4     // no interrupt required
+#define ENCODER0INDEX        5      
+#define CPR                  400    // encoder cycles per revolution
+#define CLOCKWISE            2       // direction constant
+#define COUNTER_CLOCKWISE    1       // direction constant
+
+// variables modified by interrupt handler must be declared as volatile
+volatile long encoder0Position = 0;
+volatile long interruptsReceived = 0;
+
+// track direction: 0 = counter-clockwise; 1 = clockwise
+short currentDirection = CLOCKWISE;
+
+// track last position so we know whether it's worth printing new output
+long previousPosition = 0;
+
+void setup()
+{
+
+  // inputs
+  pinMode(ENCODER0PINA, INPUT);
+  pinMode(ENCODER0PINB, INPUT);
+  pinMode(ENCODER0INDEX, INPUT);
+  
+  // interrupts
+  attachInterrupt(1, onInterrupt, RISING);
+
+  // enable diagnostic output
+  Serial.begin (9600);
+  Serial.println("\n\n\n");
+  Serial.println("Ready.");
+}
+
+void loop()
+{
+  // only display position info if has changed
+  if (encoder0Position != previousPosition )
+  {
+    Serial.print(encoder0Position, DEC);
+    Serial.print("\t");
+    Serial.print(currentDirection == CLOCKWISE ? "clockwise" : "counter-clockwise");
+    Serial.print("\t");
+    Serial.println(interruptsReceived, DEC);
+    previousPosition = encoder0Position;
+  }
+}
+
+// interrupt function needs to do as little as possible
+void onInterrupt()
+{
+  // read both inputs
+  int a = digitalRead(ENCODER0PINA);
+  int b = digitalRead(ENCODER0PINB);
+  
+  //read the index:
+  int index = digitalRead(ENCODER0INDEX);
+  
+  if (a == b )
+  {
+    // b is leading a (counter-clockwise)
+    encoder0Position--;
+    currentDirection = COUNTER_CLOCKWISE;
+  }
+  else
+  {
+    // a is leading b (clockwise)
+    encoder0Position++;
+    currentDirection = CLOCKWISE;
+  }
+
+  // track 0 to 400
+  encoder0Position = encoder0Position % CPR;
+
+  //If we hit the index this time, reset encoder position,
+  //and number of interrupts received.
+  if (index != 0) {
+     encoder0Position = 0; 
+     interruptsReceived = 0;
+  }
+
+  // track the number of interrupts
+  interruptsReceived++;
+}
