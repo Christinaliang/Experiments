03d6fb4aedd2ae4fe8e03945d69fc569e5fcc3fd
Taylor Nightingale <nighting47@gmail.com>
2015-04-08 16:21:17 -0700

    Updated the interface to allow each part to run without killing everything else.

--- UI/ManualInterface.py
+++ UI/ManualInterface.py
@@ -20,21 +20,32 @@ class MARSControlConsole(Frame):
             sensorTable = Label(image=sampletableImage)
             sensorTable.image = sampletableImage
             sensorTable.grid(row=0, column=0, rowspan=2, sticky=W, padx=5)
+        except tkinter.TclError:
+            ""
+        try:
             # Fake the video feed
             sampleVideoImage = PhotoImage(file="images/VideoSample.png")
             videoFeed = Label(image=sampleVideoImage)
             videoFeed.image = sampleVideoImage
             videoFeed.grid(row=0, column=1, sticky=N, padx=5, pady=5)
+        except tkinter.TclError:
+            ""
+        try:
             # Fake the controls console
-            sampleControlAreaImage = PhotoImage(file="images/ControlsConsole.png")
             inputArea = Label(image=sampleControlAreaImage)
             inputArea.image=sampleControlAreaImage
             inputArea.grid(row=1, column=2, sticky=N+S+E+W, padx=5, pady=5)
+        except tkinter.TclError:
+            ""
+        try:
             # Fake an arena map
             sampleLocationImage = PhotoImage(file="images/LocationSample.png")
             locationvisualizer = Label(image=sampleLocationImage)
             locationvisualizer.image = sampleLocationImage
             locationvisualizer.grid(row=0, column=2, padx=5, pady=5)
+        except tkinter.TclError:
+            ""
+        try:
             # Fake the transport status visualizer
             SampleTransportViz = PhotoImage(file="images/TransporterStatusImage.png")
             transportVisualizer = Label(image=SampleTransportViz)
