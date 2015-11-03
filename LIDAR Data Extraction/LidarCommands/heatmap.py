f5e342e7ea2e6642825bd073c28028be1f67e507
taylorstadeli <stadeli16@up.edu>
2015-11-02 17:44:27 -0800

    Create heatmap.py
    Given x, y, and intensity data generate a heatmap

---
+++ LIDAR Data Extraction/LidarCommands/heatmap.py
@@ -0,0 +1,37 @@
+#===============================================================================
+# heatmap.py
+# heatmap generator given, x, y, and intensity data
+# author: Taylor Stadeli
+# date: Oct. 22, 2015
+#
+#===============================================================================
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+#generate some random data
+x = []
+y = []
+z = []
+intensity = []
+for i in range (0, 2000):
+    x.append(i)
+for i in range (0, 2000):
+    y.append(i)
+for j in range(0,2000):
+    z = []
+    for i in range (0, 2000):
+        z.append(random.randint(1, 100))
+    intensity.append(z)
+
+
+#setup the 2D grid with Numpy
+x, y = np.meshgrid(x, y)
+
+#convert intensity (list of lists) to a numpy array for plotting
+intensity = np.array(intensity)
+
+#plug the data into pcolormesh,
+plt.pcolormesh(x, y, intensity)
+plt.colorbar() #need a colorbar to show the intensity scale
+plt.show() 
