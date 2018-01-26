e84030766fbc4e65120e97480d76a0c6d6e3efdf
Alex Reinemann <Reineman18@up.edu>
2018-01-25 19:57:13 -0800

    conversion step 2
    Converted lidar_scan_test.py and lidar_servo_driver.py

--- LIDAR Data Extraction/LidarCommands/lidar_scan_test.py
+++ LIDAR Data Extraction/LidarCommands/lidar_scan_test.py
@@ -1,18 +1,18 @@
 __author__ = 'Jaimiey'
 __version__ = 1
 
-from threads import main as runTest
+from .threads import main as runTest
 import datetime as dt
 
 # Get information from user regarding number of test cases
 try:
-    ip = int(raw_input("Number of scans to do: "))
+    ip = int(input("Number of scans to do: "))
 except ValueError:
-    print "Not a valid number. Exiting"
+    print("Not a valid number. Exiting")
     raise SystemExit
 
 # run x tests, prompting for input
 for i in range(ip):
-    for j in range(3): print "----------------------------------"
-    print "Beginning Scan {} at {}".format(i, dt.datetime.now())
+    for j in range(3): print("----------------------------------")
+    print("Beginning Scan {} at {}".format(i, dt.datetime.now()))
     runTest()
