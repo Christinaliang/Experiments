b54dc8caeaa2a01f2aecf9ad2a890cfeb607ba28
Alex Reinemann <Reineman18@up.edu>
2018-02-05 19:24:14 -0800

    Added code to read a pickle into heetmap

--- LIDAR Data Extraction/LidarCommands/heatmap.py
+++ LIDAR Data Extraction/LidarCommands/heatmap.py
@@ -8,21 +8,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import pickle
 
-#generate some random data
-x = []
-y = []
-z = []
-intensity = []
-for i in range (0, 2000):
-    x.append(i)
-for i in range (0, 2000):
-    y.append(i)
-for j in range(0,2000):
-    z = []
-    for i in range (0, 2000):
-        z.append(random.randint(1, 100))
-    intensity.append(z)
+with open('test_vectors_2018_2_5_19_3_59.dat', 'rb') as f:
+    dataArrays = pickle().load(f)
+
+
+
+x = dataArrays[0]
+y = dataArrays[1]
+z = dataArrays[2]
+
+
+##generate some random data
+#x = []
+#y = []
+#z = []
+#intensity = []
+#for i in range (0, 2000):
+#    x.append(i)
+#for i in range (0, 2000):
+#    y.append(i)
+#for j in range(0,2000):
+#    z = []
+#    for i in range (0, 2000):
+#        z.append(random.randint(1, 100))
+#    intensity.append(z)
 
 
 #setup the 2D grid with Numpy
