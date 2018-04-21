66ef3358bd81afda08cc51a9563ad8fbae0f4799
Lexden12 <lexden.s@gmail.com>
2018-04-21 04:15:31 -0700

    Update lidar_servo, Fix raspi_threads
    Servo Driver code updated to include the three servos that will be used with the mount.
    Raspi_threads essentially rolled back to when it worked. Still working on the shape of the array.

--- LIDAR Data Extraction/LidarCommands/Currently Used/arduino/lidar_servo_driver/lidar_servo_driver.ino
+++ LIDAR Data Extraction/LidarCommands/Currently Used/arduino/lidar_servo_driver/lidar_servo_driver.ino
@@ -10,39 +10,33 @@
 //include the pub/sub message format
 #include <command2ros/ScanCommand.h>
 
-Servo servo;//create a servo object
-int servoPin=9;
+Servo rohServo, raiseServo, thetaServo;//create a servo objects (roh is the scanning, theta is forwards or backwards, and raise is raising or lowering the mount.
+int rohPin=9, thetaPin=10, raisePin=11;
 int pos = 0;//integer to store position in degrees(for loop iterator) 
 const int MID = 90;
-const int START_POS = MID+30;
+const int START_POS = MID;
 const int END_POS = MID-40;
-int scanDelay = 15;//May need to increase delay for scanning...
+int scanDelay = 1000;//May need to increase delay for scanning...
 const int FRONT = 0;//for rotating LIDAR, can do front scan or back scan
 const int BACK = 180;
+const int DOWN = 0;//for the raising and lowering of the LIDAR mount.
+const int UP = 90;
 int id=0;
 bool isComplete = false;//if scan is complete, go back to initial state
-bool led = false;
 ros::NodeHandle nh;
 
 void messageCb(const command2ros::ScanCommand& scan_msg){
   if(id != scan_msg.serialID){//if we get a new message
     id = scan_msg.serialID;
     isComplete = false;//reset everything to initial state
+    //scan();
     pos=START_POS;
-    servo.write(pos);
+    rohServo.write(pos);
     if(scan_msg.scan){//and rotate to the correct direction to scan front or back
-      //rotate to front
+      thetaServo.write(FRONT);//rotate to front
     }
     else{
-      //rotate to back
-    }
-    if(led){
-      digitalWrite(LED_BUILTIN, HIGH);
-      led = !led;
-    }
-    else{
-      digitalWrite(LED_BUILTIN, LOW);
-      led = !led;
+      thetaServo.write(BACK);//rotate to back
     }
   }
 }
@@ -50,12 +44,25 @@ void messageCb(const command2ros::ScanCommand& scan_msg){
 ros::Subscriber<command2ros::ScanCommand> sub("Scan", &messageCb);
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  servo.attach(servoPin);//attach servo to pin 9
+  rohServo.attach(rohPin);//attach servo to pin 9
+  thetaServo.attach(thetaPin);//attach servo to pin 10
+  raiseServo.attach(raisePin);//attach servo to pin 11
+  raiseServo.write(UP);
   Serial.begin(57600);
   nh.initNode();
   nh.subscribe(sub);
 }
+
+/*void scan() {
+  if(pos <= END_POS){
+    pos = START_POS;
+    servo.write(pos);
+  }
+  else{
+    pos=pos+1;
+    servo.write(pos);
+  }
+}*/
 
 void scan() {
   if(pos <= END_POS){
@@ -63,12 +70,12 @@ void scan() {
   }
   if(!isComplete){
     pos -= 1;
-    servo.write(pos);
+    rohServo.write(pos);
     delay(scanDelay);
   }
   else if(isComplete){
     pos = START_POS;
-    servo.write(pos);
+    rohServo.write(pos);
   }
 }
 
